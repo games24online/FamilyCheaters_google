23ead61acf0293b866c6596d581d39a00ed0e751
games24online <137506795+games24online@users.noreply.github.com>
2023-07-08 12:03:32 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,454 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="user-scalable=no,width=device-width,initial-scale=1.0">
+  <meta http-equiv="Cache-control" content="public">
+  <title>Funn Games</title>
+  <meta name="description" content="Funn Games will make you play all night long.">
+  <meta name="keywords" content="exotic games, family cheaters, FunnGames, Funn Games">
+
+  <link rel="apple-touch-icon" sizes="180x180" href="./files/img/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="./files/img/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="./files/img/favicon-16x16.png">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet">
+  <script type="text/javascript" src="./files/jquery.min.js"></script>
+  <script defer type="text/javascript" src="./files/lang.js"></script>
+
+  <!-- part 1 -->
+  <link rel="stylesheet" href="./files/style.css" id="style1">
+
+  <!-- part 2 -->
+  <link rel="stylesheet" href="./files/reset.min.css">
+  <link rel="stylesheet" href="./files/style10.css" id="style2">
+
+  <script defer type="text/javascript" src="./files/slick.min.js"></script>
+  <link rel="stylesheet" href="./files/slick.min.css">
+  <script defer type="text/javascript" src="./files/functions10.js"></script>
+
+  <!-- =========== Cookie =========== -->
+  <link rel="stylesheet" href="./files/cookie.css">
+  <script defer type="text/javascript" src="./files/cookie.js"></script>
+</head>
+
+<body>
+  <!-- First part -->
+  <div class="view-1">
+    <div class="steps">
+      <div class="step step1 active">
+        <div class="step__container container">
+          <div class="step__content content">
+            <div class="content__bg center">
+              <div class="step__title text-uppercase multitext" data-multitext="p1-step1-1">WELCOME TO THE GAME</div>
+              <div class="step__logo"><img src="./files/img/logo.png" alt="Logo FamilyCheaters"></div>
+            </div>
+          </div>
+          <div class="step__buttons">
+            <div class="step__button">
+              <div class="btn next-btn multitext" data-multitext="p1-step1-2">NO THANKS</div>
+            </div>
+            <div class="step__button">
+              <div class="btn next-btn multitext" data-multitext="p1-step1-3">CONTINUE</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="step step2">
+        <div class="step__container container container-lg">
+          <div class="step__content content">
+            <div class="center">
+              <div class="step__logo"><img src="./files/img/logo.png" alt="Logo FamilyCheaters"></div>
+              <div class="step__title multitext" data-multitext="p1-step2-1">Choose your partner</div>
+            </div>
+            <div class="content__select-cards">
+              <div class="step__cards cards">
+                <div class="cards__item next-btn">
+                  <div class="cards__title">
+                    <span class="text-uppercase multitext" data-multitext="p1-step2-2">STEPSISTER</span>
+                  </div>
+                </div>
+                <div class="cards__item next-btn">
+                  <div class="cards__title">
+                    <span class="text-uppercase multitext" data-multitext="p1-step2-3">STEPMOM</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="step step3">
+        <div class="step__container container container-md">
+          <div class="step__content content content--mob">
+            <div class="center">
+              <div class="step__logo"><img src="./files/img/logo.png" alt="Logo FamilyCheaters"></div>
+            </div>
+            <div class="content__bg">
+              <div class="content__description multitext" data-multitext="p1-step3-1">THIS SIMULATOR IS VERY EXTREME AND
+                IS NOT FOR THE AVERAGE PERSON</div>
+              <div class="step__title step__title--color text-uppercase multitext" data-multitext="p1-step3-2">WOULD YOU
+                LIKE TO CONTINUE?</div>
+            </div>
+          </div>
+          <div class="step__button step__button-last">
+            <div class="btn my-link multitext" data-multitext="p1-step3-3">PLAY NOW</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Second part -->
+  <div class="view-2"
+    style="display:none;opacity:0;-ms-filter:'progid:DXImageTransform.Microsoft.Alpha(Opacity=0)';filter:alpha(opacity=0);-moz-opacity:0;-khtml-opacity:0;">
+    <div class="padv1215-content">
+      <div class="padv1215-step active step4" data-step="1">
+        <div class="padv1215-container">
+          <div class="padv1215-logo-big">
+            <img src="./files/logo.png" width="740" height="83" alt="Family Cheaters">
+          </div>
+          <div class="text__wrap">
+            <p class="padv1215-main-title">
+              <span class="padv1215-color multitext" data-multitext="p2-step1-1" data-text="WARNING!">
+                WARNING!
+              </span>
+            </p>
+            <p class="padv1215-text">
+              <span class="multitext" data-multitext="p2-step1-2">
+                This game makes you play all night long
+              </span>
+            </p>
+          </div>
+          <button type="button" class="padv1215-btn textBtnContinue multitext" data-multitext="p2-step1-3"
+            data-btn="start" data-step data-step-category="step" data-step-action="step 1" data-step-label="button 1">
+            PLAY NOW
+          </button>
+        </div>
+      </div>
+
+      <div class="padv1215-step step5" data-step="2">
+        <div class="padv1215-container">
+          <div class="padv1215-heading">
+            <img class="padv1215-logo" src="./files/logo.png" width="530" height="59" alt="Family Cheaters">
+            <div class="progressBar padv1215-progress-bar"></div>
+          </div>
+          <div class="text__wrap">
+            <p class="padv1215-text padding-top-30 multitext" data-multitext="p2-step2-1">
+              Before you play, we need to ask you a couple of questions.
+            </p>
+            <p class="padv1215-text multitext" data-multitext="p2-step2-2">
+              Is your door locked and are you ready to play?
+            </p>
+          </div>
+          <button type="button" class="padv1215-btn multitext" data-multitext="p2-step2-3"
+            data-btn="next">
+            YES
+          </button>
+        </div>
+      </div>
+
+      <div class="padv1215-step step6" data-step="3">
+        <div class="padv1215-container">
+          <div class="padv1215-heading">
+            <img class="padv1215-logo" src="./files/logo.png" width="530" height="59" alt="Family Cheaters">
+            <div class="progressBar padv1215-progress-bar"></div>
+          </div>
+          <div class="text__wrap">
+            <p class="padv1215-text padding-top-30 multitext" data-multitext="p2-step3-1">
+              You are going to see a multitude of explicit erotic scenes.
+            </p>
+            <p class="padv1215-text multitext" data-multitext="p2-step3-2">
+              Can you handle this?
+            </p>
+          </div>
+          <button type="button" class="padv1215-btn multitext" data-multitext="p2-step3-3" data-btn="next">
+            Yes
+          </button>
+        </div>
+      </div>
+
+      <div class="padv1215-step step7" data-step="4">
+        <div class="padv1215-container">
+          <div class="padv1215-heading">
+            <img class="padv1215-logo" src="./files/logo.png" width="530" height="59" alt="Family Cheaters" />
+            <div class="progressBar padv1215-progress-bar"></div>
+          </div>
+          <div class="text__wrap">
+            <p class="padv1215-main-title padv1215-color multitext" data-multitext="p2-step4-1">
+              FAMILY GAMES
+            </p>
+            <p class="padv1215-text multitext" data-multitext="p2-step4-2">
+              Our games might feature explicit erotic scenarios between someone you dream of, possibly stepmoms or
+              stepsisters.
+            </p>
+            <p class="padv1215-text multitext" data-multitext="p2-step4-3">
+              Is this OK with you?
+            </p>
+          </div>
+          <button type="button" class="padv1215-btn multitext" data-multitext="p2-step4-4" data-btn="next">
+            Yes
+          </button>
+        </div>
+      </div>
+
+      <div class="padv1215-step step8" data-step="5">
+        <div class="padv1215-container">
+          <div class="padv1215-heading">
+            <img class="padv1215-logo" src="./files/logo.png" width="530" height="59" alt="Family Cheaters">
+            <div class="progressBar padv1215-progress-bar"></div>
+          </div>
+          <div class="text__wrap">
+            <p class="padv1215-main-title padv1215-color multitext" data-multitext="p2-step5-1">
+              DISCLAIMER
+            </p>
+            <p class="padv1215-text multitext" data-multitext="p2-step5-2">
+              These games can be highly addicting and in some cases ruin your relationships and have a negative effect
+              on your job,
+              sleeping etc.
+            </p>
+            <p class="padv1215-text multitext" data-multitext="p2-step5-3">
+              Do you have any problems with addiction?
+            </p>
+          </div>
+          <div class="btn__wrap btn--couple">
+            <button type="button" class="padv1215-btn textBtnNo multitext" data-multitext="p2-step5-4" data-btn="next">
+              No
+            </button>
+            <button type="button" class="padv1215-btn multitext" data-multitext="p2-step5-5" data-btn="next">
+              Yes
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div class="padv1215-step step9" data-step="6">
+        <div class="padv1215-container">
+          <div class="padv1215-heading">
+            <img class="padv1215-logo" src="./files/logo.png" width="530" height="59" alt="Family Cheaters">
+            <div class="progressBar padv1215-progress-bar"></div>
+          </div>
+          <div class="text__wrap">
+            <p class="padv1215-main-title padv1215-color multitext" data-multitext="p2-step6-1">
+              AWESOME!
+            </p>
+            <p class="padv1215-text multitext" data-multitext="p2-step6-2">
+              Games are compatible with your browser.
+            </p>
+            <p class="padv1215-text multitext" data-multitext="p2-step6-3">
+              No download is required.
+            </p>
+          </div>
+          <button type="button" class="padv1215-btn multitext" data-multitext="p2-step6-4" data-btn="next">
+            CONTINUE
+          </button>
+        </div>
+      </div>
+
+      <div class="padv1215-step step10" data-step="7">
+        <div class="padv1215-container">
+          <div class="padv1215-heading">
+            <img class="padv1215-logo" src="./files/logo.png" width="530" height="59" alt="Family Cheaters" />
+            <div class="progressBar padv1215-progress-bar"></div>
+          </div>
+          <div class="text__wrap">
+            <p class="padv1215-main-title padv1215-color multitext" data-multitext="p2-step7-1">
+              ALMOST DONE!
+            </p>
+            <p class="padv1215-text multitext" data-multitext="p2-step7-2">
+              You need to choose your username and passwords to create your own character.
+            </p>
+            <p class="padv1215-text multitext" data-multitext="p2-step7-3">
+              Do you understand and accept our terms?
+            </p>
+          </div>
+          <button type="button" class="padv1215-btn multitext" data-multitext="p2-step7-4" data-btn="next">
+            Yes
+          </button>
+        </div>
+      </div>
+
+      <div class="padv1215-step step11" data-step="8">
+        <div class="padv1215-container padv1215-container-sm">
+          <div class="padv1215-carousel-wrap">
+            <div class="padv1215-heading">
+              <img class="padv1215-logo" src="./files/logo.png" width="530" height="59" alt="Family Cheaters">
+              <div class="progressBar padv1215-progress-bar"></div>
+            </div>
+            <div id="carouselCharacters" class="padv1215-characters-carousel">
+              <div class="padv1215-slide">
+                <div class="padv1215-picture">
+                  <img id="character1" src="#" width="488" height="538" alt="Lisa">
+                </div>
+                <div class="padv1215-name multitext" data-multitext="p2-step8-2">
+                  Sakura
+                </div>
+              </div>
+              <div class="padv1215-slide">
+                <div class="padv1215-picture">
+                  <img id="character2" src="#" width="488" height="538" alt="Adriana">
+                </div>
+                <div class="padv1215-name multitext" data-multitext="p2-step8-3">
+                  Hana
+                </div>
+              </div>
+              <div class="padv1215-slide">
+                <div class="padv1215-picture">
+                  <img id="character3" src="#" width="488" height="538" alt="Character 4">
+                </div>
+                <div class="padv1215-name multitext" data-multitext="p2-step8-4">
+                  Yumi
+                </div>
+              </div>
+              <div class="padv1215-slide">
+                <div class="padv1215-picture">
+                  <img id="character4" src="#" width="488" height="538" alt="Character 5">
+                </div>
+                <div class="padv1215-name multitext" data-multitext="p2-step8-5">
+                  Aiko
+                </div>
+              </div>
+              <div class="padv1215-slide">
+                <div class="padv1215-picture">
+                  <img id="character5" src="#" width="488" height="538" alt="Character 6">
+                </div>
+                <div class="padv1215-name multitext" data-multitext="p2-step8-6">
+                  Miyuki
+                </div>
+              </div>
+            </div>
+          </div>
+          <button type="button" class="padv1215-btn textBtnSelect multitext" data-multitext="p2-step8-7" data-btn="next">
+            Select
+          </button>
+        </div>
+      </div>
+
+      <div class="padv1215-step step12" data-step="9">
+        <div class="padv1215-container">
+          <div class="padv1215-heading">
+            <img class="padv1215-logo" src="./files/logo.png" width="530" height="59" alt="Family Cheaters" />
+            <div class="progressBar padv1215-progress-bar"></div>
+          </div>
+          <p class="padv1215-text">
+            <span class="multitext" data-multitext="p2-step9-1">What erotic fantasy would you like to try with
+              your character?</span>
+          </p>
+          <div class="padv1215-tiles-container">
+            <button type="button" class="padv1215-btn mrgn multitext" data-btn="next"
+              data-multitext="p2-step9-2">Domination
+            </button>
+            <button type="button" class="padv1215-btn mrgn multitext" data-btn="next"
+              data-multitext="p2-step9-3">Letting Go of Control
+            </button>
+            <button type="button" class="padv1215-btn mrgn multitext" data-btn="next" data-multitext="p2-step9-4">
+            </button>
+            <button type="button" class="padv1215-btn mrgn multitext" data-btn="next"
+              data-multitext="p2-step9-5">Intimacy in a car
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div class="padv1215-step step13" data-step="10">
+        <div class="padv1215-container">
+          <div class="padv1215-heading">
+            <img class="padv1215-logo" src="./files/logo.png" width="530" height="59" alt="Family Cheaters" />
+            <div class="progressBar padv1215-progress-bar"></div>
+          </div>
+          <div class="text__wrap">
+            <p class="padv1215-main-title padv1215-color multitext" data-multitext="p2-step10-1">
+              SUCCESS!
+            </p>
+            <p class="padv1215-text multitext" data-multitext="p2-step10-2">
+              All that's left is for you to verify your age by creating account.
+            </p>
+            <p class="padv1215-text multitext" data-multitext="p2-step10-3">
+              You won’t be charged!
+            </p>
+          </div>
+          <button type="button" class="padv1215-btn multitext" data-multitext="p2-step10-4"
+            data-btn="next">
+            Play Now
+          </button>
+        </div>
+      </div>
+
+      <div class="padv1215-step step14" data-step="11">
+        <div class="padv1215-container">
+          <p class="padv1215-title multitext" data-multitext="p2-step11-1">
+            Processing your answers
+          </p>
+          <div class="padv1215-check-items">
+            <p class="multitext" data-multitext="p2-step11-2">
+              Evaluating your answers...
+            </p>
+            <p class="multitext" data-multitext="p2-step11-3">
+              Analyzing daily registration limits...
+            </p>
+            <p class="multitext" data-multitext="p2-step11-4">
+              Registrations remaining: 2
+            </p>
+            <p>
+              <span class="multitext" data-multitext="p2-step11-5">All games compatible with your browser
+              </span>
+            </p>
+            <p class="multitext" data-multitext="p2-step11-6">
+              ACCOUNT APPROVED!
+            </p>
+          </div>
+          <button type="button" class="padv1215-btn padv1215-btn--hidden multitext"
+            data-multitext="p2-step11-7" data-btn="next">
+            Play Game
+          </button>
+        </div>
+      </div>
+
+      <div class="padv1215-step step15" data-step="12">
+        <div class="padv1215-container-3 padv1215-mainframe-2">
+          <!-- ============= Registration Form =============-->
+          <?php $partner = 'vpx'; include($_SERVER["DOCUMENT_ROOT"]. "/secureform/index.php"); ?>
+          <!-- END Registration Form  -->
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- =================== Footer =================== -->
+  <footer>
+    <ul>
+      <li><a target="_blank" href="./privacy.html" class="multitext" data-multitext="footer-1">
+        </a></li>
+      <li class="multitext" data-multitext="footer-2">
+      </li>
+    </ul>
+  </footer>
+
+  <div class="cookies">
+    <div class="cookies__text">
+      <strong class="multitext" data-multitext="cookie-1">
+      </strong>
+      <br>
+      <span class="multitext" data-multitext="cookie-2"></span>
+      <a href="privacy.html" class="underline multitext" data-multitext="cookie-3">
+      </a>
+      <span class="multitext" data-multitext="cookie-4"></span>
+      <br>
+      <span class="multitext" data-multitext="cookie-5"></span>
+    </div>
+    <div class="cookies__button cookies__ok"><strong>
+        <span class="multitext" data-multitext="cookie-6"></span>
+      </strong>,
+      <span class="multitext" data-multitext="cookie-7"></span>
+    </div>
+    <div class="cookies__close cookies__ok"></div>
+  </div>
+
+  <!-- part 1 -->
+  <script type="text/javascript" src="./files/script.js"></script>
+
+</body>
+
+</html>
